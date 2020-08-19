0bd053ac75a508f2c093608450bbe4654a60b6d5
andresgr20 <andresgr2094@hotmail.com>
2020-08-18 21:10:38 -0400

    Fixed navbar to become mobile friendly

--- src/Education.js
+++ src/Education.js
@@ -9,21 +9,23 @@ export default function Education(){
     return(
     <>
         <h2>Education</h2>
-        <Card 
-            company={edu[12].card.company}
-            title={edu[12].card.position}
-            image={edu[12].card.image}
-            location={edu[12].card.location}
-            date={edu[12].card.duration}
-            description={edu[12].card.description}
-            />
-    
+        {edu.filter(item => item.card.type === 'school').map((item)=>{
+                        return <Card 
+                        image={item.card.image}
+                        company={item.card.company}
+                        title={item.card.position}
+                        date={item.card.duration}
+                        location={item.card.location}
+                        description={item.card.description}
+                        />
+                })}
+
         <h2>Certifations</h2>
         {cert.map((item) => {
                 return <ProjectCard
-                    title={item.card.name}
-                    date={item.card.finished}
-                    description={item.card.description}
+                    title={item.info.name}
+                    date={item.info.date}
+                    link={item.info.download}
                 />
             })}  
     </>
--- src/NavBar.js
+++ src/NavBar.js
@@ -1,46 +1,59 @@
-import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './NavBar.css'
+import React,{useState} from 'react';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
 import InstagramIcon from '@material-ui/icons/Instagram';
 import EmailIcon from '@material-ui/icons/Email';
 import GitHubIcon from '@material-ui/icons/GitHub';
-import './NavBar.css'
+import {Collapse,Navbar,NavbarToggler,Nav,NavItem,NavLink} from 'reactstrap';
 import {Link} from "react-router-dom";
 
-export default function NavBar(){
-    return(
-        <nav className="nav-bar">
-            <ul className="external-links">
-                <li>
-                    <a href="https://github.com/andresgr20" alt="Github link"><GitHubIcon/></a>
-                </li>
-                <li>
-                    <a href="https://www.linkedin.com/in/andres-garcia-rodriguez-ba02b863" alt="LinkedIn link"><LinkedInIcon/></a>
-                </li>
-                <li>
-                    <a href="mailto:andresgr2094@gmail.com" alt="Email"><EmailIcon/></a>
-                </li>
-                <li>
-                    <a href="https://www.instagram.com/andresgr20/"><InstagramIcon/></a>
-                </li>
-            </ul>           
-            <ul className="internal-links">
-                <li>
-                    <Link to="/">About</Link>
-                </li>
-                <li>
-                    <Link to="/education">Education & Certifications</Link>
-                </li>
-                <li>
-                    <Link to="/experience">Experience</Link>
-                </li>
-                <li>
-                    <Link to="/projects">Projects</Link>
-                </li>
-                <li>
-                    <a href={require("./assets/resume.pdf")} download="Resume">Resume</a>
-                </li>
-            </ul>
-        </nav> 
 
+export default function NavBar(){
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggle = () => setIsOpen(!isOpen);
+
+    return(
+        <header>
+            <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" light>
+                <Nav className="mr-auto icons">
+                    <NavItem>
+                        <NavLink href="https://github.com/andresgr20"><GitHubIcon/></NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink href="https://www.linkedin.com/in/andres-garcia-rodriguez-ba02b863"><LinkedInIcon/></NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink href="mailto:andresgr2094@gmail.com"><EmailIcon/></NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink href="https://www.instagram.com/andresgr20/"><InstagramIcon/></NavLink>
+                    </NavItem>
+                </Nav>
+                <Nav navbar>              
+                    <NavbarToggler onClick={toggle} className="mr-2" />
+                    <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={isOpen} navbar>
+                        <ul className="navbar-nav flex-grow">
+                        <NavItem>
+                            <NavLink tag={Link} className="text-dark" to="/">About</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink tag={Link} className="text-dark" to="/education" >Education</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink tag={Link} className="text-dark" to="/experience">Experience</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink tag={Link} className="text-dark" to="/projects">Projects</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink href={require("./assets/resume.pdf")} download="Resume" className="text-dark">Resume</NavLink>
+                        </NavItem>
+                    </ul>
+                    </Collapse>
+                </Nav>
+            </Navbar>
+        </header>
     );
 }
