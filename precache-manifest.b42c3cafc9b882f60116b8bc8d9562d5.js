4f096df058c9aac542ab529a812ddcd851f96c3c
andresgr20 <andresgr2094@hotmail.com>
2020-09-04 16:39:06 -0400

    Updates

---
+++ precache-manifest.b42c3cafc9b882f60116b8bc8d9562d5.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a09510deacaaae99ff1572f868b24b29",
+    "url": "/index.html"
+  },
+  {
+    "revision": "318625b062153a54d851",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "5132b8f2c771153cb118",
+    "url": "/static/css/main.2daf6b19.chunk.css"
+  },
+  {
+    "revision": "318625b062153a54d851",
+    "url": "/static/js/2.78884634.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.78884634.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5132b8f2c771153cb118",
+    "url": "/static/js/main.be25530a.chunk.js"
+  },
+  {
+    "revision": "ba126735810163618457",
+    "url": "/static/js/runtime-main.3374ac37.js"
+  },
+  {
+    "revision": "19a267ab7444cd55ac5ad9d83b91ddf9",
+    "url": "/static/media/CLF.19a267ab.pdf"
+  },
+  {
+    "revision": "22057bffa114bcf0f56c9693701b995b",
+    "url": "/static/media/Community.22057bff.pdf"
+  },
+  {
+    "revision": "7f670374a65535358a1f3f866d1ebfbe",
+    "url": "/static/media/borrowell.7f670374.png"
+  },
+  {
+    "revision": "e7bdc7ba7cf372597c07fda8fb674799",
+    "url": "/static/media/chalk.e7bdc7ba.png"
+  },
+  {
+    "revision": "6835af2fac895b3960bddf38a345a52b",
+    "url": "/static/media/glow.6835af2f.png"
+  },
+  {
+    "revision": "ce2a9ac5480b9a1fa946be527248b352",
+    "url": "/static/media/kognitive.ce2a9ac5.jpeg"
+  },
+  {
+    "revision": "ce777798adec17146bd5ea5900c1a5c0",
+    "url": "/static/media/mef.ce777798.png"
+  },
+  {
+    "revision": "094da0622a2ade11f8019aca3c3abcbe",
+    "url": "/static/media/pic.094da062.jpg"
+  },
+  {
+    "revision": "7ebe79678aadcfedb7868722a3cb0c72",
+    "url": "/static/media/rbc.7ebe7967.svg"
+  },
+  {
+    "revision": "7f79138e4b174da6874964e025429ef7",
+    "url": "/static/media/resume.7f79138e.pdf"
+  },
+  {
+    "revision": "31e73eb2dc09c1a42ad8b5da826cb863",
+    "url": "/static/media/waterloo.31e73eb2.png"
+  }
+]);
